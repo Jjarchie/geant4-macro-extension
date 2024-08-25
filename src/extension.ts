bcb571b9b9439236e3530adf6e99274951eba3bd
Jay Archer <imjayarcher@gmail.com>
2024-08-25 17:49:32 +1000

    removing temp command

--- src/extension.ts
+++ src/extension.ts
@@ -6,15 +6,6 @@ import {g4macrocommands} from './g4macrocommands';
 
 export function activate(context: vscode.ExtensionContext) {
 	
-	console.log('Activating Geant4 Macro Extension...');
-
-	// A placeholder for regenerating the UI command cache
-	const regenerateCacheCmd = vscode.commands.registerCommand('extension.regenerateCache', () => {
-		vscode.window.showInformationMessage('TODO: Implement regenerateCache command.');
-	});
-
-	context.subscriptions.push(regenerateCacheCmd);
-
 	// Import the completions JSON
 	const completionsPath = path.join(context.extensionPath, 'completions.json');
 
