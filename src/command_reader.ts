106361f8950e13c9d86114d8e5e1d21c284d4187
Jay Archer <imjayarcher@gmail.com>
2024-10-17 22:40:03 +1100

    fixing custom directory read

--- src/command_reader.ts
+++ src/command_reader.ts
@@ -1,6 +1,7 @@
 
 import * as fs from 'fs';
 import * as rd from 'readline';
+import { CommentThreadCollapsibleState } from 'vscode';
 
 interface Parameter {
     name: string;
@@ -22,12 +23,15 @@ interface Commands {
 
 export function processCommands(path: string) {
 
-    const reader = rd.createInterface(fs.createReadStream("example_custom.txt"));
+    console.log("Processing commands from " + path);
+
+    const reader = rd.createInterface(fs.createReadStream(path));
 
     const directoryPathSpecififier = "Command directory path : ";
-    const guidanceSpecififier = "Guidance : ";
+    const guidanceSpecififier = "Guidance :";
     const subdirectoriesSpecifier = " Sub-directories :";
-    const parametersSpecifier = "Parameters :";
+    const parametersSpecifier = "Parameter :";
+    const commandSpecifier = "Command /";
 
     const commands: Commands = {};
     let currentCommand: Command | null = null;
@@ -36,44 +40,57 @@ export function processCommands(path: string) {
     let readingGuidance: boolean = false;
     let readingParameter: boolean = false;
 
-    reader.on("line", (line: string) => {
+    console.log("Reading commands...");
 
-        if (line.startsWith(guidanceSpecififier)) {
-            readingGuidance = true;
-        }
+    reader.on("line", (line: string) => {
 
         if (line.startsWith(directoryPathSpecififier)) {
 
             readingGuidance = false;
             readingParameter = false;
 
-            commandPath = line.slice(directoryPathSpecififier.length).split("/");
+            let thisCommand = commands;
 
+            if (currentCommand != null && currentCommand.command != '') {
+
+                // Add to commands
+                for (let i = 0; i < commandPath.length - 1; i++) {
+
+                    thisCommand = thisCommand[commandPath[i]].children;
+                }
+
+                thisCommand[commandPath[commandPath.length - 1]] = currentCommand;
+
+                console.log("Added command ", currentCommand);
+            }
+
+            // Get the command path, removing the whitespace at the start and end
+            commandPath = line.slice(directoryPathSpecififier.length).split("/").slice(1, -1);
+
+            // Initialise the current command
             currentCommand = { command: commandPath[commandPath.length - 1], guidance: "", parameters: [], children: {} };
         }
 
-        if (line.startsWith(subdirectoriesSpecifier)) {
+        else if (line.startsWith(subdirectoriesSpecifier)) {
             readingGuidance = false;
         }
 
-        if (line.startsWith(parametersSpecifier)) {
+        else if (line.startsWith(parametersSpecifier)) {
             readingParameter = true;
-            const parameterName = line.slice(parametersSpecifier.length);
+            readingGuidance = false;
+
+            const parameterName = line.slice(parametersSpecifier.length + 1);
 
             if (currentCommand != null)
                 currentCommand.parameters.push({ name: parameterName, type: "", omittable: false });
         }
 
+        else if (line.startsWith(guidanceSpecififier))
+            readingGuidance = true;
 
-        if (readingGuidance && currentCommand != null) {
 
-            if (currentCommand.guidance != "")
-                currentCommand.guidance += "\n";
+        else if (readingParameter && currentCommand != null) {
 
-            currentCommand.guidance = line;
-        }
-
-        if (readingParameter && currentCommand != null) {
             if (line.startsWith(" Parameters type"))
                 currentCommand.parameters[currentCommand.parameters.length - 1].type = line.split(" : ")[1];
 
@@ -81,6 +98,17 @@ export function processCommands(path: string) {
                 currentCommand.parameters[currentCommand.parameters.length - 1].omittable = line.split(" : ")[1] == "True";
         }
 
+        else if (readingGuidance && currentCommand != null) {
+
+            if (currentCommand.guidance != "")
+                currentCommand.guidance += "\n";
+
+            currentCommand.guidance += line;
+
+        }
+
     });
+
+    console.log(commands);
 
 }
